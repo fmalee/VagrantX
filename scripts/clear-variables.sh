6b60990ec4c7c8a0f0d50c9ea69d0fd351b29724
Joey Lee <lixueli@gmail.com>
2016-04-18 00:30:10 +0800

    清理变量的时候判断文件是否存在。从而适应没有装PHP7.0的环境

--- scripts/clear-variables.sh
+++ scripts/clear-variables.sh
@@ -1,4 +1,10 @@
-# Clear The Old Environment Variables
+#!/usr/bin/env bash
 
-sed -i '/# Set Homestead Environment Variable/,+1d' /home/vagrant/.profile
-sed -i '/env\[.*/,+1d' /etc/php/7.0/fpm/php-fpm.conf
+# Clear The Old Environment Variables
+if [ -e /home/vagrant/.profile ]; then
+	sed -i '/# Set VagrantX Environment Variable/,+1d' /home/vagrant/.profile
+fi
+
+if [ -e /etc/php/7.0/fpm/php-fpm.conf ]; then
+	sed -i '/env\[.*/,+1d' /etc/php/7.0/fpm/php-fpm.conf
+fi
