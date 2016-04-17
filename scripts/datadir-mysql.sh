57ec3333a68d5f16f0484b19329015015e9ccb0d
Joey Lee <lixueli@gmail.com>
2016-04-18 00:31:16 +0800

    简化在清理和增加`apparmor.d`增加权限的sed方法

--- scripts/datadir-mysql.sh
+++ scripts/datadir-mysql.sh
@@ -4,12 +4,11 @@ if [ -d $1 ]; then
 	# 变更datadir目录
 	sed -i "s?^datadir.*?datadir = $1?" /etc/mysql/my.cnf
 	# 清理之前添加的apparmor目录权限
-	sed -i 'N;N;s/# Change_Datadir_end\n/# Change_Datadir_end/' /etc/apparmor.d/usr.sbin.mysqld
-	sed -i ":begin; /# Change_Datadir_begin/,/Change_Datadir_end/ { /Change_Datadir_end/! { $! { N; b begin }; }; s/# Change_Datadir_begin.*Change_Datadir_end//; };" /etc/apparmor.d/usr.sbin.mysqld
+	sed -i '/# Allow new data dir access/,+3d' /etc/apparmor.d/usr.sbin.mysqld
 
 	if [ $1 != "/var/lib/mysql" ]; then
 		# 在apparmor中添加目录权限
-		sed -i "\/var\/lib\/mysql\/\*\* rwk,/a\# Change_Datadir_begin\n  $1/ r,\n  $1\/** rwk,\n# Change_Datadir_end" /etc/apparmor.d/usr.sbin.mysqld
+		sed -i "\/var\/lib\/mysql\/\*\* rwk,/a\ \n# Allow new data dir access\n  $1\/ r,\n  $1\/** rwk," /etc/apparmor.d/usr.sbin.mysqld
 
 		/etc/init.d/apparmor restart
 	fi
